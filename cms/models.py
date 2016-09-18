d4c1e980e3802d8f08a75bc7aac21a713ef82edb
Eraldo Energy <eraldo@eraldo.org>
2016-09-18 15:41:12 +0200

    Added a generic content page for wagtail cms.

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailcore.models import Page
 
-# Create your models here.
+
+class ContentPage(Page):
+    content = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('content', classname="full", ),
+    ]
